efdd28e1c72d627579cfbbcdbc21d92ec8c41dc7
Preethi <preethidovala@gmail.com>
2021-11-21 19:15:33 +0530

    Delete remote-server.sh

--- remote-server.sh
+++
@@ -1,4 +0,0 @@
-sudo amazon-linux-extras install docker -y
-sudo systemctl start docker
-sudo docker login -u $USERNAME -p $PASSWORD
-sudo docker run -itd -p 8000:80 devopstrainer/java-mvn-repos:$BUILDNUMBER
